4fdd34fa132f6288b3ddabbad4a1cbef12ce7f64
kale <kale@kalmenn.fr>
2025-01-18 14:28:03 +0100

    chore: Create a nix flake
    For now it only provides easy access to the flatbuffers compiler

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "RoBoard-Core";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              flatbuffers
+            ];
+          };
+        }
+      );
+    };
+}
+
